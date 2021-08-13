dedbc8af3ad7635bda4406bb69c39c4659e118c1
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2021-08-13 15:16:54 -0500

    Refactor for updateQuality method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,40 +14,37 @@ class Shop {
     this.items.forEach((item) => {
       if (item.name === "Sulfuras, Hand of Ragnaros") return item.quality = 80;
 
-      if (item.name === "Aged Brie") {
-        item.sellIn--;
-        item.quality++;
-        if (item.sellIn < 0) {
+      switch (item.name) {
+        case "Aged Brie":
+          item.sellIn--;
           item.quality++;
-        }
-      } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn > 10) {
-          item.quality++;
-        } else if (item.sellIn > 5 && item.sellIn <= 10) {
-          item.quality = item.quality + 2;
-        } else if (item.sellIn > 0 && item.sellIn <= 5) {
-          item.quality = item.quality + 3;
-        } else {
-          item.quality = 0;
-        }
-      } else {
-        item.sellIn--;
-        item.quality--;
-        if (item.name === "Conjured Mana Cake") item.quality--;
-        
-        if (item.sellIn < 0) {
+          if (item.sellIn < 0) item.quality++;
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          if (item.sellIn > 10) {
+            item.quality++;
+          } else if (item.sellIn > 5 && item.sellIn <= 10) {
+            item.quality = item.quality + 2;
+          } else if (item.sellIn > 0 && item.sellIn <= 5) {
+            item.quality = item.quality + 3;
+          } else {
+            item.quality = 0;
+          }
+          break;
+        default:
+          item.sellIn--;
           item.quality--;
           if (item.name === "Conjured Mana Cake") item.quality--;
-        }
-        if (item.quality < 0) {
-          item.quality = 0;
-        }
+        
+          if (item.sellIn < 0) {
+            item.quality--;
+            if (item.name === "Conjured Mana Cake") item.quality--;
+          }
+          if (item.quality < 0) item.quality = 0;
+          break;
       }
-
-      if (item.quality > 50) {
-        item.quality = 50;
-      }
-
+      
+      if (item.quality > 50) item.quality = 50;
     })
 
     return this.items;
